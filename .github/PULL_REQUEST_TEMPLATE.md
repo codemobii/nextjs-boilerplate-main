0e5742d4aa0784db0e461240733ebadc5126ab59
John <45017704+codemobii@users.noreply.github.com>
2022-07-07 09:48:25 +0100

    Initial commit

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,12 @@
+### What does this PR do?
+1. ..
+2. ..
+3. ..
+
+
+### list up the UI check list for the client-side
+1. ..
+2. ..
+3. ..
+
+### Screenshots(if needed)
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Layout from "components/common/Layout";
+import Greeting from "components/home/Greeting";
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <Greeting message={"Hello World with test"} />
+    <Link href="/about">
+      <a>About</a>
+    </Link>
+  </Layout>
+);
+
+export default IndexPage;
